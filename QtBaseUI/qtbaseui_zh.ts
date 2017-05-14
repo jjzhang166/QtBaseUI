9ef74da554ebf88a5ee41e57db307408f14f36cc
彼岸 <qshore@qq.com>
2017-05-14 22:15:11 +0800

    样式修改

---
+++ QtBaseUI/qtbaseui_zh.ts
@@ -0,0 +1,80 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>FrameHelperPrivate</name>
+    <message>
+        <location filename="Src/FrameHelperPrivate.cpp" line="276"/>
+        <source>Max</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Src/FrameHelperPrivate.cpp" line="283"/>
+        <source>Restore</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainTitleBar</name>
+    <message>
+        <location filename="Src/MainTitleBar.ui" line="14"/>
+        <source>MainTitleBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Src/MainTitleBar.ui" line="94"/>
+        <source>min</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Src/MainTitleBar.ui" line="104"/>
+        <source>max</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Src/MainTitleBar.ui" line="114"/>
+        <source>close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MessageBox</name>
+    <message>
+        <location filename="Src/MessageBox.ui" line="14"/>
+        <source>MessageBox</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Src/MessageBox.cpp" line="45"/>
+        <source>Info Message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Src/MessageBox.cpp" line="50"/>
+        <source>Error Message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Src/MessageBox.cpp" line="55"/>
+        <source>Warn Message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Src/MessageBox.cpp" line="60"/>
+        <location filename="Src/MessageBox.cpp" line="68"/>
+        <source>Yes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Src/MessageBox.cpp" line="63"/>
+        <location filename="Src/MessageBox.cpp" line="70"/>
+        <source>Question Message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Src/MessageBox.cpp" line="76"/>
+        <source>Kindly Reminder</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
